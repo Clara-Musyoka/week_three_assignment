5ee6ace02e76ace6efc891e85e5cc106018e636b
Clara Musenya Musyoka <CMMUSYOKA@safaricom.co.ke>
2022-11-07 18:36:51 +0300

    Add the interaction for the form

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
+
+
+  
