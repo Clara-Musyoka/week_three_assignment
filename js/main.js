ef27e213b847f08d924ef0dd73c6a6885b35dcd9
Clara Musenya Musyoka <CMMUSYOKA@safaricom.co.ke>
2022-11-07 21:38:42 +0300

    Add js for the portfolio section to display what th images are about

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
   $(document).ready(function(){
+    /*Form section*/
     $("form#form34A").submit(function(event){
       // event.preventDefault();
       var name = $("input#MERGE1").val();
@@ -11,7 +12,7 @@
       }
       
     });
-
+/*What we do section*/
     $("#whole-design").click(function(){
         $("#design").toggle();
         $("#design-image").toggle();
@@ -26,9 +27,58 @@
         $("#product-image").toggle();
     });
 
-    
+      //Insert white box upo hover
+ 
   
   });
+  $(document).ready(function () {
+	$("#work1").mouseover(function () {
+		$("#overlay").show();
+	}).mouseout(function () {
+		$("#overlay").hide();
+	});
+});
+$(document).ready(function () {
+	$("#work2").mouseover(function () {
+		$("#overlay2").show();
+	}).mouseout(function () {
+		$("#overlay2").hide();
+	});
+});
+$(document).ready(function () {
+	$("#work3").mouseover(function () {
+		$("#overlay3").show();
+	}).mouseout(function () {
+		$("#overlay3").hide();
+	});
+});
+$(document).ready(function () {
+	$("#work4").mouseover(function () {
+		$("#overlay4").show();
+	}).mouseout(function () {
+		$("#overlay4").hide();
+	});
+});
 
-
-  
+$(document).ready(function () {
+	$("#work5").mouseover(function () {
+		$("#overlay5").show();
+	}).mouseout(function () {
+		$("#overlay5").hide();
+	});
+	$("#work6").mouseover(function () {
+		$("#overlay6").show();
+	}).mouseout(function () {
+		$("#overlay6").hide();
+	});
+	$("#work7").mouseover(function () {
+		$("#overlay7").show();
+	}).mouseout(function () {
+		$("#overlay7").hide();
+	});
+	$("#work8").mouseover(function () {
+		$("#overlay8").show();
+	}).mouseout(function () {
+		$("#overlay8").hide();
+	});
+});
